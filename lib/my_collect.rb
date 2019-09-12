8cfe334ddf90ccea68b6e02f97ce3da046916889
Matt Plichta <bigcatplichta@gmail.com>
2019-09-12 00:15:28 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,14 +3,9 @@ require 'pry'
 def my_collect(array)
   i = 0 
   new_array = []
- 
   while i < array.length 
-    
     new_array << yield(array[i])
-    
     i += 1 
-    
   end
-  # binding.pry
   new_array
 end 
